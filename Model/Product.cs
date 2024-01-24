6f7fc8aef9834053cca93c12d4b8081b937c28c6
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-01-24 13:23:04 +0530

    Upload Product.cs

---
+++ Model/Product.cs
@@ -0,0 +1,39 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace pankaj - project . Model{
+    /// <summary> 
+    /// Represents a product entity with essential details
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Primary key for the Product 
+        /// </summary>
+        [Key]
+        [Required]
+        public Guid ProductId { get; set; }
+        /// <summary>
+        /// Name of the Product 
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Code of the Product 
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// Price of the Product 
+        /// </summary>
+        public string Price { get; set; }
+        /// <summary>
+        /// Collection navigation property representing associated 
+        /// </summary>
+        public ICollection<OrderLine> OrderLines { get; set; }
+        /// <summary>
+        /// Collection navigation property representing associated 
+        /// </summary>
+        public ICollection<Sales> Saless { get; set; }
+    }
+}
