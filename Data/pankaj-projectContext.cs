0d2423f76a896f9ed05d4bbfbf304e5d6eed622d
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-01-24 13:22:43 +0530

    Upload pankaj-projectContext.cs

---
+++ Data/pankaj-projectContext.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using pankaj-project.Model;
+using System;
+
+namespace pankaj-project.Data{
+    public class pankaj-projectContext : DbContext
+    {
+        protected override void OnConfiguring(Microsoft.EntityFrameworkCore.DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-GFUVFUP;Initial Catalog=Pankaj_project;Persist Security Info=True;user id=pankazz;password=123456;Integrated Security=true;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasKey(a => a.CustomerId);
+            modelBuilder.Entity<Order>().HasKey(a => a.OrderID);
+            modelBuilder.Entity<OrderLine>().HasKey(a => a.OrderLineId);
+            modelBuilder.Entity<Product>().HasKey(a => a.ProductId);
+            modelBuilder.Entity<OrderStatus>().HasKey(a => a.OrderStatusId);
+            modelBuilder.Entity<Country>().HasKey(a => a.Name);
+            modelBuilder.Entity<Sales>().HasKey(a => a.SalesId);
+            modelBuilder.Entity<Customer>().HasOne(a => a.Country).WithMany(b => b.Customers).HasForeignKey(c => c.CountryName);
+            modelBuilder.Entity<Order>().HasOne(a => a.Customer).WithMany(b => b.Orders).HasForeignKey(c => c.CustomerId);
+            modelBuilder.Entity<Order>().HasOne(a => a.OrderStatus).WithMany(b => b.Orders).HasForeignKey(c => c.OrderStatusId);
+            modelBuilder.Entity<OrderLine>().HasOne(a => a.Order).WithMany(b => b.OrderLines).HasForeignKey(c => c.OrderId);
+            modelBuilder.Entity<OrderLine>().HasOne(a => a.Product).WithMany(b => b.OrderLines).HasForeignKey(c => c.ProductId);
+            modelBuilder.Entity<Sales>().HasOne(a => a.Product).WithMany(b => b.Saless).HasForeignKey(c => c.ProductId);
+        }
+
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderLine> OrderLine { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<OrderStatus> OrderStatus { get; set; }
+        public DbSet<Country> Country { get; set; }
+        public DbSet<Sales> Sales { get; set; }
+    }
+}
