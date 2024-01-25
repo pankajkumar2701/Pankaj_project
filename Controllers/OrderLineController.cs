f2ae4af8d5a8cb8256503be6be20f6c78bd90160
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-01-25 14:53:14 +0530

    Upload OrderLineController.cs

--- Controllers/OrderLineController.cs
+++ Controllers/OrderLineController.cs
@@ -32,18 +32,17 @@ namespace pankaj-project.Controllers{
         }
 
         /// <summary>Retrieves a list of orderlines based on specified filters</summary>
-        /// <param name="filters">The filter criteria in JSON format. Use the following format:                                
-        /// [
-        ///   {
-        ///     "Property": "PropertyName",
-        ///     "Operator": "Equal",
-        ///     "Value": "FilterValue"
-        ///   }
-        /// ]
+        /// <param name="filters">The filter criteria in JSON format. Use the following format: [{"Property": "PropertyName", "Operator": "Equal", "Value": "FilterValue"}] </param>
+        /// <returns>The filtered list of orderlines</returns>
         [HttpGet]
         public IActionResult Get([FromQuery] string filters)
         {
-            var filterCriteria = JsonHelper.Deserialize<List<FilterCriteria>>(filters);
+            List<FilterCriteria> filterCriteria = null;
+            if (!string.IsNullOrEmpty(filters))
+            {
+                filterCriteria = JsonHelper.Deserialize<List<FilterCriteria>>(filters);
+            }
+
             var query = _context.OrderLine.AsQueryable();
             var result = FilterService<OrderLine>.ApplyFilter(query, filterCriteria);
             return Ok(result);
